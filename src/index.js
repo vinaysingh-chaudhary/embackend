e5b4b0f9cc1de7ce2a67e0d62d5b0b2e39c674d5
Vinay chaudhary <vinay.heyworld@gmail.com>
2024-06-05 13:38:59 +0530

    Updated Package.json (#3)

--- src/index.js
+++ src/index.js
@@ -2,16 +2,14 @@ import dotenv from 'dotenv'
 import dbConnect from "./database/index.js";
 import app from './app.js';
 
-dotenv.config({
-    path: './env'
-}); 
+dotenv.config(); 
 
 dbConnect()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running at port: ${process.env.PORT}`); 
     }); 
-})
+}) 
 .catch((error) => {
     console.log(`Failed to connect with Mongodb, ${error}`); 
 })
